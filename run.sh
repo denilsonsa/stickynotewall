1792e1e22a9f73cc5baf536faa799a17b57200df
Denilson Sá <denilsonsa@gmail.com>
2011-07-19 18:31:34 -0300

    Binding to '' address, so that the dev_appserver is also accessible from other computers.

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-dev_appserver.py --high_replication .
+# Binding to '' address, so that it is also accessible from other computers.
+
+dev_appserver.py --high_replication --address='' .
