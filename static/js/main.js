293dcc7e505cef082d60deab135c5062875dc079
Denilson Sá <denilsonsa@gmail.com>
2011-07-21 22:29:54 -0300

    Grouped functions inside JavaScript objects 'frontend', 'backend' and 'events'.

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
 // JSLint comments:
-/*global document, window, XMLHttpRequest */
-/*jslint sloppy: true, white: true, onevar: false, plusplus: true, maxerr: 50, maxlen: 78, indent: 4 */
+/*global document, window, XMLHttpRequest, console, FormData */
+/*jslint undef: true, sloppy: true, white: true, onevar: false, plusplus: true, maxerr: 50, maxlen: 78, indent: 4 */
 
 
 // document.querySelectorAll()
@@ -37,144 +37,181 @@
 // http://dev.w3.org/html5/spec/elements.html#embedding-custom-non-visible-data-with-the-data-attributes
 
 
-var XHR_instance;
+var frontend, backend, events;
 
+frontend = {
+	// Frontend functions care about HTML elements.
+	// They know nothing about server communication.
 
-function clear_wall() {
-	var notes = document.querySelectorAll('.wall > .note');
-	var i;
-	for (i = 0; i < notes.length; i++) {
-		notes[i].parentNode.removeChild(notes[i]);
-	}
-}
+	'clear_wall': function() {
+		// Removes all .note elements from the .wall element.
+		// Useful before reloading all notes.
 
-function new_note_element(obj) {
-	var note_div = document.createElement('div');
-	note_div.setAttribute('draggable', 'true');
-	note_div.setAttribute('class', 'note');
-	note_div.style.left = obj.x + 'px';
-	note_div.style.top = obj.y + 'px';
-	note_div.style.width = obj.width + 'px';
-	note_div.style.height = obj.height + 'px';
-	note_div.style.zIndex = obj.z;
-
-	note_div.addEventListener('dragstart', dragstart_experiment, false);
-
-	var text_div = document.createElement('div');
-	text_div.setAttribute('class', 'text');
-
-	var text = document.createTextNode(obj.text);
-
-	text_div.appendChild(text);
-	note_div.appendChild(text_div);
-
-	return note_div;
-}
-
-function create_notes_from_json(json_obj) {
-	var wall = document.getElementsByClassName('wall')[0];
-	var i;
-	for (i = 0; i < json_obj.length; i++) {
-		wall.appendChild(new_note_element(json_obj[i]));
-	}
-}
-
-
-function load_from_ajax() {
-	if (XHR_instance) {
-		XHR_instance.abort();
-	} else {
-		XHR_instance = new XMLHttpRequest();
-	}
-
-	XHR_instance.onreadystatechange = function() {
-		if (this.readyState === 4 && this.status === 200) {
-			var json_obj = JSON.parse(this.responseText);
-
-			clear_wall();
-			create_notes_from_json(json_obj);
+		var notes = document.querySelectorAll('.wall > .note');
+		var i;
+		for (i = 0; i < notes.length; i++) {
+			notes[i].parentNode.removeChild(notes[i]);
 		}
-		// else... do nothing
-	};
+	},
 
-	XHR_instance.open('GET', '/ajax/get_notes');
-	XHR_instance.send();
-}
+	'new_note_element': function(obj) {
+		// Receives a Note JavaScript object and creates the HTML elements that
+		// represent such object.
+		//
+		// Returns the created .note element (which must later be added to the
+		// document tree)
 
-function move_note_ajax_post(id, x, y, z) {
-	var XHR = new XMLHttpRequest();
-	var formdata = new FormData();
+		var note_div = document.createElement('div');
+		note_div.setAttribute('draggable', 'true');
+		note_div.setAttribute('class', 'note');
+		note_div.style.left = obj.x + 'px';
+		note_div.style.top = obj.y + 'px';
+		note_div.style.width = obj.width + 'px';
+		note_div.style.height = obj.height + 'px';
+		note_div.style.zIndex = obj.z;
 
-	formdata.append('id', id);
-	formdata.append('x', x);
-	formdata.append('y', y);
-	formdata.append('z', z);
+		note_div.addEventListener('dragstart', events.dragstart_experiment, false);
 
-	XHR.onreadystatechange = function() {
-		if (this.readyState === 4 && this.status === 200) {
-			var json_obj = JSON.parse(this.responseText);
-			// TODO: implement me...
+		var text_div = document.createElement('div');
+		text_div.setAttribute('class', 'text');
+
+		var text = document.createTextNode(obj.text);
+
+		text_div.appendChild(text);
+		note_div.appendChild(text_div);
+
+		return note_div;
+	},
+
+	'create_notes_from_array': function(json_obj) {
+		// Receives an Array of Notes, and then creates and attaches each note
+		// to the document tree.
+
+		var wall = document.getElementsByClassName('wall')[0];
+		var i;
+		for (i = 0; i < json_obj.length; i++) {
+			wall.appendChild(frontend.new_note_element(json_obj[i]));
 		}
-		// else... do nothing
 	}
 
-	XHR.open('POST', '/ajax/move_note');
-	XHR.send(formdata);
-}
+};
 
 
-function dragstart_experiment(ev) {
-	console.log("dragstart", this, ev);
-	ev.stopPropagation();
 
-	ev.dataTransfer.setData('text/plain', 'Blah blah');
-}
-function dragenter_experiment(ev) {
-	console.log("dragenter", this, ev);
-}
+backend = {
+	// Backend functions care about server communication, and about calling the
+	// frontend functions after a server response.
 
+	// Using only one XHR object for reloading
+	'XHR_reload': null,
 
-function accepting_drag(ev) {
-	// This function is a dragover event handler
+	'reload_notes_using_ajax': function() {
+		// Tries reloading all notes from server, using AJAX.
 
-	// In this context, preventDefault() means YesIamAcceptingTheDragPlease()
-	ev.preventDefault();
+		if (backend.XHR_reload) {
+			backend.XHR_reload.abort();
+		} else {
+			backend.XHR_reload = new XMLHttpRequest();
+		}
 
-	// stopPropagation() doesn't seem really needed, though.
-	ev.stopPropagation();
-}
+		backend.XHR_reload.onreadystatechange = function() {
+			if (this.readyState === 4 && this.status === 200) {
+				var json_obj = JSON.parse(this.responseText);
 
-function drop_experiment(ev) {
-	// Only one of stopPropagation() or preventDefault() is actually needed
-	ev.stopPropagation();
-	ev.preventDefault();
+				frontend.clear_wall();
+				frontend.create_notes_from_array(json_obj);
+			}
+			// else... do nothing
+		};
 
-	var tmp = document.createElement('div');
-	var rect = this.getBoundingClientRect();
-	var left = ev.clientX - rect.left - this.clientLeft;
-	var top = ev.clientY - rect.top - this.clientTop;
-	tmp.setAttribute('style', 'position:absolute; width: 2px; height: 2px; top: '+top+'px; left: '+left+'px; background: red;');
-	this.appendChild(tmp);
-}
+		backend.XHR_reload.open('GET', '/ajax/get_notes');
+		backend.XHR_reload.send();
+	},
 
+	'move_note_ajax_post': function(id, x, y, z) {
+		// Huh... I don't know... This function isn't used yet.
 
-function on_load_handler() {
-	var button = document.getElementById('load_from_ajax_button');
-	if (button) {
-		button.addEventListener('click', load_from_ajax, false);
+		var XHR = new XMLHttpRequest();
+		var formdata = new FormData();
+
+		formdata.append('id', id);
+		formdata.append('x', x);
+		formdata.append('y', y);
+		formdata.append('z', z);
+
+		XHR.onreadystatechange = function() {
+			if (this.readyState === 4 && this.status === 200) {
+				var json_obj = JSON.parse(this.responseText);
+				// TODO: implement me...
+			}
+			// else... do nothing
+		};
+
+		XHR.open('POST', '/ajax/move_note');
+		XHR.send(formdata);
 	}
 
-	button = document.getElementById('clear_wall');
-	if (button) {
-		button.addEventListener('click', clear_wall, false);
+};
+
+events = {
+	// These functions handle all UI events, and usually call other functions
+	// from the backend and frontend.
+
+	'dragstart_experiment': function(ev) {
+		console.log("dragstart", this, ev);
+		ev.stopPropagation(); // not really needed, but it makes sense here.
+
+		ev.dataTransfer.setData('text/plain', 'Blah blah');
+	},
+
+	'dragenter_experiment': function(ev) {
+		//console.log("dragenter", this, ev);
+	},
+
+
+	'accepting_drag': function(ev) {
+		// This function is a dragover event handler
+
+		// In this context, preventDefault() means YesIamAcceptingTheDragPlease()
+		ev.preventDefault();
+
+		// stopPropagation() doesn't seem really needed, though.
+		ev.stopPropagation();
+	},
+
+	'drop_experiment': function(ev) {
+		// Only one of stopPropagation() or preventDefault() is actually needed
+		ev.stopPropagation();
+		ev.preventDefault();
+
+		var tmp = document.createElement('div');
+		var rect = this.getBoundingClientRect();
+		var left = ev.clientX - rect.left - this.clientLeft + this.scrollLeft;
+		var top = ev.clientY - rect.top - this.clientTop + this.scrollTop;
+		tmp.setAttribute('style', 'position:absolute; width: 2px; height: 2px; top: '+top+'px; left: '+left+'px; background: red;');
+		this.appendChild(tmp);
+	},
+
+
+	'on_load_handler': function() {
+		var button = document.getElementById('load_from_ajax_button');
+		if (button) {
+			button.addEventListener('click', backend.reload_notes_using_ajax, false);
+		}
+
+		button = document.getElementById('clear_wall');
+		if (button) {
+			button.addEventListener('click', frontend.clear_wall, false);
+		}
+
+
+
+		var wall = document.getElementsByClassName('wall')[0];
+		wall.addEventListener('dragover', events.accepting_drag, false);
+		wall.addEventListener('dragenter', events.dragenter_experiment, false);
+		wall.addEventListener('drop', events.drop_experiment, false);
 	}
 
+};
 
-
-	var wall = document.getElementsByClassName('wall')[0];
-	wall.addEventListener('dragover', accepting_drag, false);
-	wall.addEventListener('dragenter', dragenter_experiment, false);
-	wall.addEventListener('drop', drop_experiment, false);
-}
-
-window.addEventListener('load', on_load_handler, false);
+window.addEventListener('load', events.on_load_handler, false);
